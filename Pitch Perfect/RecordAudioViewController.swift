0e093adcb46fb2a19b6998ee829cb53af24d2b5a
Joe McGuinness <joe.mcguinness@thisisglobal.com>
2017-11-02 21:33:13 +0000

    sets up avaudiorecorder to record audio

--- Pitch Perfect/RecordAudioViewController.swift
+++ Pitch Perfect/RecordAudioViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordAudioViewController: UIViewController {
 
@@ -14,6 +15,7 @@ class RecordAudioViewController: UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
     let recordLabelString = "Tap to record"
+    var audioRecorder: AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +23,24 @@ class RecordAudioViewController: UIViewController {
         stopRecordingButton.isEnabled = false
         recordingLabel.text = recordLabelString
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func recordAudio(_ sender: AnyObject) {
-        print("test")
         stopRecordingButton.isEnabled = true
         recordButton.isEnabled = false
         recordingLabel.text = "Recording in progress..."
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord
+        audioRecorder.record()
         
     }
     @IBAction func stopRecordingAudio(_ sender: Any) {
